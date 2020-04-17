88b7d03feed1819db47ebe7c438472e4cdbfd362
mario <corporalpiquiblanco@gmail.com>
2020-04-17 20:43:02 +0200

    reformat; add tracking of score components

--- case_evaluator.py
+++ case_evaluator.py
@@ -24,11 +24,19 @@ class CaseEvaluator:
         if available_train_tags:
             for tag in available_train_tags:
                 for i in range(4):
-                    self.cases.append({'tag': tag, 'rotation': i, 'map': SimpleMap(self.map)})
+                    self.cases.append(
+                        {"tag": tag, "rotation": i, "map": SimpleMap(self.map)}
+                    )
             # self.cases.append({'tag': available_train_tags[0], 'rotation': 0, 'map': SimpleMap(self.map)})
         else:
-            self.cases.append({'tag': self.map.available_tags[0], 'rotation': 0, 'map': SimpleMap(self.map)})
-            self.cases[0]['map'].summary.append('No available destination for trains')
+            self.cases.append(
+                {
+                    "tag": self.map.available_tags[0],
+                    "rotation": 0,
+                    "map": SimpleMap(self.map),
+                }
+            )
+            self.cases[0]["map"].summary.append("No available destination for trains")
 
     def pick_best_case(self):
         for case in self.cases:
@@ -39,7 +47,7 @@ class CaseEvaluator:
             self.best_case = tools.rargmax(self.values)
 
     def evaluate_case(self, case):
-        simple_map = case['map']
+        simple_map = case["map"]
         self.try_add_card(case)
         key = self.map.get_new_key(
             simple_map.map_structure, simple_map.train_positions, simple_map.collisions
@@ -47,7 +55,7 @@ class CaseEvaluator:
         if key not in self.map.state_values.keys():
             self.map.discover_state(key)
         key_entry = self.map.state_values[key]
-        value = key_entry['metric'] / key_entry['games'] + simple_map.points
+        value = key_entry["metric"] / key_entry["games"] + simple_map.points
         return value
 
     def trains_next(self):
@@ -55,74 +63,94 @@ class CaseEvaluator:
         for train in self.map.train_positions:
             tag = self.map.train_positions[train][0:2]
             gate = self.map.train_positions[train][2]
-            if gate not in ['s', 'l']:
+            if gate not in ["s", "l"]:
                 vector = self.map.gate_to_vector[int(gate)]
                 new_position = tools.move_a_tag(tag, vector)
                 available_train_tags.append(new_position)
-        result = list(set(available_train_tags).intersection(set(self.map.available_tags)))
+        result = list(
+            set(available_train_tags).intersection(set(self.map.available_tags))
+        )
         return result
 
     def try_move_trains(self, case):
         for train in [1, 2, 3, 4]:
             moving = 1
             total_moves = -1
-            start_position = case['map'].train_positions[train]
-            new_position = '000'
-            if start_position != 'collided' and start_position[2] != 's':
+            start_position = case["map"].train_positions[train]
+            new_position = "000"
+            if start_position != "collided" and start_position[2] != "s":
                 while moving == 1:
-                    position = case['map'].train_positions[train]
+                    position = case["map"].train_positions[train]
                     total_moves += 1
-                    if position in case['map'].tracks.keys():
-                        new_position = case['map'].tracks[position][0]
-                        through = case['map'].tracks[position][1]
-                        if through in case['map'].train_positions.values():
-                            colliding_train = [x for x in case['map'].train_positions
-                                               if case['map'].train_positions[x] == through][0]
-                            self.map.vprint('Trains {} and {} have collided!'.format(train, colliding_train))
-                            case['map'].train_positions[train] = 'collided'
-                            case['map'].train_positions[colliding_train] = 'collided'
-                            case['map'].collisions += 1
+                    if position in case["map"].tracks.keys():
+                        new_position = case["map"].tracks[position][0]
+                        through = case["map"].tracks[position][1]
+                        if through in case["map"].train_positions.values():
+                            colliding_train = [
+                                x
+                                for x in case["map"].train_positions
+                                if case["map"].train_positions[x] == through
+                            ][0]
+                            self.map.vprint(
+                                "Trains {} and {} have collided!".format(
+                                    train, colliding_train
+                                )
+                            )
+                            case["map"].train_positions[train] = "collided"
+                            case["map"].train_positions[colliding_train] = "collided"
+                            case["map"].collisions += 1
                             moving = 0
                         else:
-                            case['map'].train_positions[train] = new_position
+                            case["map"].train_positions[train] = new_position
                     else:
                         moving = 0
-                        if new_position[2] == 's':
+                        if new_position[2] == "s":
                             station = new_position[0:2]
-                            if station not in case['map'].new_stations:
-                                case['map'].new_stations.append(station)
-                end_position = case['map'].train_positions[train]
+                            if station not in case["map"].new_stations:
+                                case["map"].new_stations.append(station)
+                end_position = case["map"].train_positions[train]
                 if start_position != end_position:
-                    case['map'].summary.append('Train {} moved from {} to {} in {} moves'.format(
-                        train, start_position, end_position, total_moves)
+                    case["map"].summary.append(
+                        "Train {} moved from {} to {} in {} moves".format(
+                            train, start_position, end_position, total_moves
+                        )
                     )
-                case['map'].points += total_moves
+                case["map"].points += total_moves
 
     def try_add_card(self, case):
-        case['map'].map_structure[case['tag']] = (self.map.card_in_use[0], case['rotation'])
-        case['map'].summary.append("{} adds a card in position {}".format(self.map.player_name, case['tag']))
+        case["map"].map_structure[case["tag"]] = (
+            self.map.card_in_use[0],
+            case["rotation"],
+        )
+        case["map"].summary.append(
+            "{} adds a card in position {}".format(self.map.player_name, case["tag"])
+        )
         self.try_update_paths(case)
         self.try_move_trains(case)
 
     def try_update_paths(self, case):
-        used_tags = case['map'].map_structure.keys()
+        used_tags = case["map"].map_structure.keys()
         newcard = self.map.card_in_use[1]
-        new_card = newcard[case['rotation']]
+        new_card = newcard[case["rotation"]]
         for direction in self.map.directions:
-            neighbour_tag = tools.move_a_tag(case['tag'], self.map.directions[direction])
+            neighbour_tag = tools.move_a_tag(
+                case["tag"], self.map.directions[direction]
+            )
             if neighbour_tag in used_tags:
-                ncard, nrotation = case['map'].map_structure[neighbour_tag]
+                ncard, nrotation = case["map"].map_structure[neighbour_tag]
                 if nrotation > 0:
                     neighbour_card = tools.rotations(self.map.cards[ncard])[nrotation]
                 else:
                     neighbour_card = self.map.cards[ncard]
                 all_gates = self.map.paths_to_add[direction]
                 for gate_pair in all_gates:
-                    start_from = case['tag'] + str(gate_pair[0])
+                    start_from = case["tag"] + str(gate_pair[0])
                     through_from = neighbour_tag + str(gate_pair[1])
-                    end_from = neighbour_tag + str(tools.get_other(neighbour_card, gate_pair[1]))
+                    end_from = neighbour_tag + str(
+                        tools.get_other(neighbour_card, gate_pair[1])
+                    )
                     start_to = neighbour_tag + str(gate_pair[1])
-                    through_to = case['tag'] + str(gate_pair[0])
-                    end_to = case['tag'] + str(tools.get_other(new_card, gate_pair[0]))
-                    case['map'].tracks[start_from] = (end_from, through_from)
-                    case['map'].tracks[start_to] = (end_to, through_to)
+                    through_to = case["tag"] + str(gate_pair[0])
+                    end_to = case["tag"] + str(tools.get_other(new_card, gate_pair[0]))
+                    case["map"].tracks[start_from] = (end_from, through_from)
+                    case["map"].tracks[start_to] = (end_to, through_to)
--- game.py
+++ game.py
@@ -12,30 +12,39 @@ class OneGame:
         self.greedy = greedy
         self.state_values = state_values
         self.station_finishers = {
-            '03': [],
-            '15': [],
-            '35': [],
-            '54': [],
-            '52': [],
-            '40': [],
-            '20': []
+            "03": [],
+            "15": [],
+            "35": [],
+            "54": [],
+            "52": [],
+            "40": [],
+            "20": [],
         }
         self.station_ranks = {
-            '03': 0,
-            '15': 0,
-            '35': 0,
-            '54': 0,
-            '52': 0,
-            '40': 0,
-            '20': 0
+            "03": 0,
+            "15": 0,
+            "35": 0,
+            "54": 0,
+            "52": 0,
+            "40": 0,
+            "20": 0,
         }
-        self.maps = {player: Map(player, verbose, greedy, state_values[player],
-                                 self.station_finishers, self.station_ranks) for player in players}
+        self.maps = {
+            player: Map(
+                player,
+                verbose,
+                greedy,
+                state_values[player],
+                self.station_finishers,
+                self.station_ranks,
+            )
+            for player in players
+        }
 
     def run_game(self):
         while self.turn_counter < 16:
             if self.verbose:
-                print('Turn {} begins.'.format(self.turn_counter))
+                print("Turn {} begins.".format(self.turn_counter))
             for player in self.players:
                 self.maps[player].player_move()
             self.turn_counter += 1
@@ -47,13 +56,23 @@ class OneGame:
 class GameSeries:
     def __init__(self, verbose, greedy, players):
         self.state_values = {}
-        self.state_visits = pd.DataFrame(columns=['game', 'player', 'turn', 'key'])
+        self.state_visits = pd.DataFrame(columns=["game", "player", "turn", "key"])
         self.verbose = verbose
         self.players = players
         self.greedy = greedy
         self.state_values = {player: {} for player in players}
         self.wins = {player: 0 for player in players}
-        self.all_results = pd.DataFrame(columns=['game', 'greedy', 'player', 'states', 'win', 'points', 'collisions'])
+        self.all_results = pd.DataFrame(
+            columns=[
+                "game",
+                "greedy",
+                "player",
+                "states",
+                "win",
+                "points",
+                "collisions",
+            ]
+        )
         self.game_number = 1
 
     def vprint(self, txt):
@@ -68,43 +87,64 @@ class GameSeries:
         for player in self.players:
             player_points = sum(one_game.maps[player].points)
             player_collisions = one_game.maps[player].collisions
-            self.vprint('Player {} has finished the game with {} points from trains moving and {} from stations'.format(
-                player, one_game.maps[player].points[0], one_game.maps[player].points[1]
-            )
+            self.vprint(
+                "Player {} has finished the game with {} points from trains moving and {} from stations".format(
+                    player,
+                    one_game.maps[player].points[0],
+                    one_game.maps[player].points[1],
+                )
             )
             self.state_values[player] = one_game.maps[player].state_values
             if player_points == max_points:
                 self.wins[player] += 1
-                self.vprint('Player {} won!'.format(player))
+                self.vprint("Player {} won!".format(player))
                 the_win = 1
             else:
                 the_win = 0
             for i in range(len(one_game.maps[player].move_keys)):
                 key = one_game.maps[player].move_keys[i]
-                small_df = pd.DataFrame({'game': self.game_number,
-                                         'player': player,
-                                         'turn': i+1,
-                                         'key': key},
-                                        index=[0])
-                self.state_visits = self.state_visits.append(small_df, ignore_index=True)
+                small_df = pd.DataFrame(
+                    {
+                        "game": self.game_number,
+                        "player": player,
+                        "turn": i + 1,
+                        "key": key,
+                    },
+                    index=[0],
+                )
+                self.state_visits = self.state_visits.append(
+                    small_df, ignore_index=True
+                )
                 num = player_points - one_game.maps[player].move_scores[i]
                 if key in one_game.maps[player].state_values:
-                    one_game.maps[player].state_values[key]['metric'] += num
-                    one_game.maps[player].state_values[key]['games'] += 1
+                    one_game.maps[player].state_values[key]["metric"] += num
+                    one_game.maps[player].state_values[key]["games"] += 1
                 else:
-                    print('Something is not uppppe')
+                    print("Something is not uppppe")
                     one_game.maps[player].state_values[key] = {}
-                    one_game.maps[player].state_values[key]['metric'] = num
-                    one_game.maps[player].state_values[key]['games'] = 1
-            states = len([x for x in self.state_values[player] if self.state_values[player][x]['games'] > 1])
-            to_append = pd.DataFrame({'game': self.game_number,
-                                      'greedy': self.greedy,
-                                      'player': player,
-                                      'states': states,
-                                      'win': the_win,
-                                      'points': player_points,
-                                      'collisions': player_collisions},
-                                     index=[0])
+                    one_game.maps[player].state_values[key]["metric"] = num
+                    one_game.maps[player].state_values[key]["games"] = 1
+            states = len(
+                [
+                    x
+                    for x in self.state_values[player]
+                    if self.state_values[player][x]["games"] > 1
+                ]
+            )
+            to_append = pd.DataFrame(
+                {
+                    "game": self.game_number,
+                    "greedy": self.greedy,
+                    "player": player,
+                    "states": states,
+                    "win": the_win,
+                    "tracks": one_game.maps[player].points[0],
+                    "stations": one_game.maps[player].points[0],
+                    "points": player_points,
+                    "collisions": player_collisions,
+                },
+                index=[0],
+            )
             self.all_results = self.all_results.append(to_append, ignore_index=True)
         self.game_number += 1
 
@@ -116,10 +156,22 @@ class GameSeries:
         # mean and median of visits per level
 
     def resolve_game(self):
-        with open('results.csv', 'a') as f:
+        with open("results.csv", "a") as f:
             self.all_results.to_csv(f, header=f.tell() == 0)
-        with open('state_visits.csv', 'a') as f:
+        with open("state_visits.csv", "a") as f:
             self.state_visits.to_csv(f, header=f.tell() == 0)
-        self.state_visits = pd.DataFrame(columns=['game', 'player', 'turn', 'key'])
-        self.all_results = pd.DataFrame(columns=['game', 'greedy', 'player', 'states', 'win', 'points', 'collisions'])
+        self.state_visits = pd.DataFrame(columns=["game", "player", "turn", "key"])
+        self.all_results = pd.DataFrame(
+            columns=[
+                "game",
+                "greedy",
+                "player",
+                "states",
+                "win",
+                "tracks",
+                "stations",
+                "points",
+                "collisions",
+            ]
+        )
         pickle.dump(self, open("game_series.p", "wb"))
